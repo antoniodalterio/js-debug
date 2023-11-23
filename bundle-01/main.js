9b272bcda9fd16b6b3875416f61614b9b108f900
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-23 14:25:47 +0100

    bundle1: esercizio 1

---
+++ bundle-01/main.js
@@ -0,0 +1,58 @@
+/*******************************************************************************
+
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+
+    Inserisci i tuoi commenti con le risposte, sotto ciascun esercizio
+
+*******************************************************************************/
+
+// ESERCIZIO 1
+
+/* 
+for (let i = 0; i > 5; i++) {
+  console.log(i);
+}
+
+1) è un ciclo contatore che cicla la variabile i  maggiore di 5
+cosi come è  scritto non entreremo mai nel ciclo perchè  i non è maggioredi 5 essendo uguale a 0
+
+per eseguire il ciclo i sarà minore di 5
+
+
+
+*/
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// // ESERCIZIO 2
+// function addIfEven(num) {
+//     if (num % 2 = 0) {
+//         return num + 5;
+//     }
+//     return num;
+// }
+
+// // ESERCIZIO 3
+// function loopToFive() {
+//     for (let i = 0, i < 5, i++) {
+//         console.log(i);
+//     }
+// }
+
+// // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+// function displayEvenNumbers() {
+//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//     let evenNumbers = [];
+//     for (let i = 0; i < numbers.length - 1; i++;) {
+//         if (numbers % 2 = 0); {
+//             evenNumbers.push(i);
+//         }
+//         return evenNumbers;
+//     }
+// }
+// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
