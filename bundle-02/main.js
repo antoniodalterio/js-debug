22453dbca8b321b552a27e8fbb83c92ea60ba510
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-23 15:59:51 +0100

    bundle2:  esercizio 5

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -9,111 +9,129 @@
 ​
 *******************************************************************************/
 
-
 // ESERCIZIO 1
-function checkAge() {
-    const myAge = 32;
-    const message = '';
+// function checkAge() {
+//   const myAge = 32;
+//   const message = '';
 
-    if (myAge < 18) {
-        message = `Sei troppo giovane! Hai ${myAge} anni!`;
-    } else {
-        message = 'Hai più di 18 anni!';
-    }
-}
-checkAge();
+//   if (myAge < 18) {
+//     message = `Sei troppo giovane! Hai ${myAge} anni!`;
+//   } else {
+//     message = 'Hai più di 18 anni!';
+//   }
+// }
+// checkAge();
 
 // ESERCIZIO 2
-function printColorsNumber() {
-    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-}
-printColorsNumber();
-
+// function printColorsNumber() {
+//   const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//   console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+// }
+// printColorsNumber();
 
 // ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+// function addNumbers() {
+//   const userNumber = prompt('Inserisci un numero');
+//   const total = userNumber + 12;
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
-
+//   console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
 
 // ESERCIZIO 4
-function checkAccess() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+// function checkAccess() {
+//   const addresses = [
+//     'mymail@mail.com',
+//     'yourmail@mail.com',
+//     'hermail@mail.com',
+//     'hismail@mail.com',
+//   ];
+//   const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+//   let grantAccess = 'false';
 
-    if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
-    }
+//   if (addresses.includes(userEmail)) {
+//     grantAccess = 'true';
+//   }
 
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
-    }
-}
-checkAccess();
-
+//   if (grantAccess === true) {
+//     console.log('Accesso consentito!');
+//   } else {
+//     console.log('Accesso negato!');
+//   }
+// }
+// checkAccess();
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
+
+/* 
 function checkAccessImproved() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+  const addresses = [
+    'mymail@mail.com',
+    'yourmail@mail.com',
+    'hermail@mail.com',
+    'hismail@mail.com',
+  ];
 
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+  const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+  let grantAccess = 'false';
 
-    for (let i = 0; i < addresses.length; i++) {
-        const email = addresses[i];
+  for (let i = 0; i < addresses.length; i++) {
+    const email = addresses[i];
 
-        if (userEmail.length > 5) {
-
-            if (email === userEmail) {
-                grantAccess = 'true';
-
-            }
-
-        }
-
-        if (grantAccess) {
-            console.log('Accesso consentito!');
-        } else {
-            console.log('Accesso negato!');
-        }
+    if (userEmail.length > 5) {
+      if (email === userEmail) {
+        grantAccess = 'true';
+      }
     }
-    checkAccessImproved();
+
+    if (grantAccess) {
+      console.log('Accesso consentito!');
+    } else {
+      console.log('Accesso negato!');
+    }
+    
+  }
+
+checkAccessImproved();
 
 
+5) l'esercizio controlla le email salvante nell'array
+al utente viene chiesto di inserire un email 
+- se l'email è presente nell'array il grantAccess diventa true
+
+!1) l'errore era che il grantAccess non era scritto in valore booleano ma in stringa
 
 
+*/
 
+function checkAccessImproved() {
+  const addresses = [
+    'mymail@mail.com',
+    'yourmail@mail.com',
+    'hermail@mail.com',
+    'hismail@mail.com',
+  ];
 
+  const userEmail = prompt('Inserisci il tuo indirizzo email');
 
+  let grantAccess = false;
 
+  for (let i = 0; i < addresses.length; i++) {
+    const email = addresses[i];
 
+    if (userEmail.length > 5) {
+      if (email === userEmail) {
+        grantAccess = true;
+      }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (grantAccess) {
+      console.log('Accesso consentito!');
+    } else {
+      console.log('Accesso negato!');
+    }
+  }
+}
+checkAccessImproved();
